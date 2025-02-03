c451a9f59b8d9e98a2fb7618751204e070dd87a7
Dprived2 <bcruz4957@stu.neisd.net>
2025-02-03 11:28:50 -0800

    i commit pls
    Co-authored-by: AvaaMedina <AvaaMedina@users.noreply.github.com>

--- src/Board.java
+++ src/Board.java
@@ -3,13 +3,13 @@ import java.awt.Color;
 
 public class Board {
     
-    Property[][] properties = new Property[10][10];
+    Property[][] properties = new Property[11][11];
     
 
     public Board(){
         Display.properties = properties;
         properties[0][0] = new Property(new Color(0,225,0), "Go", 0,0,0,false,0,false, 0);
-        properties[0][1] = new Property(new Color(102,51,0), "Mediterranean Avenue", 60, 50, 0, false, 2, false, 0);
+        properties[1][0] = new Property(new Color(102,51,0), "Mediterranean Avenue", 60, 50, 0, false, 2, false, 0);
         properties[0][3] = new Property(new Color(0,0,0), "Community Chest", 0, 0, 0, false, 0, false, 0);
         properties[0][2] = new Property(new Color(102,51,0), "Baltic", 60, 50, 0, false, 4, false, 0);
         properties[0][3] = new Property(new Color (0,0,0),"Income tax",0,0,0,false,0,false,0);
@@ -18,16 +18,16 @@ public class Board {
         properties[0][3] = new Property(new Color(0,0,0), "Chance", 0, 0, 0, false, 0, false, 0);
         properties[0][6] = new Property(new Color(0,153,204), "Vermont Avenut", 100,0,0,false,6,false, 0);
         properties[0][7] = new Property(new Color(0,153,204), "Connecticut Avenue", 120, 50, 0, false, 8, false, 0);
-        properties[0][4] = new Property(new Color(0,0,0),"Jail", 0, 0, 0, false, 0, false, 0);
-        properties[0][8] = new Property(new Color(204,68,204), "St. Charles Place",  140,100, 0, false, 10, false, 0);
-        properties[0][3] = new Property(new Color(0,0,0), "Electric Company",  150, 2, 0, false, 0, false, 0);
-        properties[0][9] = new Property(new Color(204,68,204), "States Avenue",  140,100,0,false,100,false,0);
-        properties[0][10] = new Property(new Color(204,68,204), "Virginia Avenue",  160,100,0,false,12,false,0);
-        properties[0][3] = new Property(new Color(0,0,0),"Pennsylvania Railroad",200,0,0,false,25,false,0);
-        properties[0][11] = new Property(new Color(204,68,204), "St. James Place", 180,100,0,false,14,false,0);
-        properties[0][3] = new Property(new Color(0,0,0), "Community Chest", 0, 0, 0, false, 0, false, 0);
-        properties[0][12] = new Property(new Color(204,68,204), "Tennessee Avenue", 180,100,0,false,14,false,0);
-        properties[0][13] = new Property(new Color(204,68,204), "New York Avenue", 200,100,0,false,16,false,0);
+        properties[0][8] = new Property(new Color(0,0,0),"Jail", 0, 0, 0, false, 0, false, 0);
+        properties[0][9] = new Property(new Color(204,68,204), "St. Charles Place",  140,100, 0, false, 10, false, 0);
+        properties[0][10] = new Property(new Color(0,0,0), "Electric Company",  150, 2, 0, false, 0, false, 0);
+        properties[1][10] = new Property(new Color(204,68,204), "States Avenue",  140,100,0,false,100,false,0);
+        properties[2][10] = new Property(new Color(204,68,204), "Virginia Avenue",  160,100,0,false,12,false,0);
+        properties[3][10] = new Property(new Color(0,0,0),"Pennsylvania Railroad",200,0,0,false,25,false,0);
+        properties[4][10] = new Property(new Color(204,68,204), "St. James Place", 180,100,0,false,14,false,0);
+        properties[5][10] = new Property(new Color(0,0,0), "Community Chest", 0, 0, 0, false, 0, false, 0);
+        properties[6][10] = new Property(new Color(204,68,204), "Tennessee Avenue", 180,100,0,false,14,false,0);
+        properties[7][10] = new Property(new Color(204,68,204), "New York Avenue", 200,100,0,false,16,false,0);
         
 
 
